adb228847aa0ecfe494f02365c5c152675a708aa
Cristian Silva <cristian123105@gmail.com>
2021-01-30 12:27:00 -0300

    chore: initial config of orm and server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import routes from './routes';
+import './database/index';
+
+const app = express();
+
+const port = process.env.PORT || 3333;
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(port);
+
+// eslint-disable-next-line no-console
+console.log(`🏠 Server running on port: ${port}`);
