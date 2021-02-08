92fcf34f2c236b495cd56ed19888f216b6cb2cbf
Cristian Silva <cristian123105@gmail.com>
2021-02-08 19:23:14 -0300

    feat: adicionando validaçao do token

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import UserController from './controllers/UserController';
+import { auth } from './middlewares/tokenValidation';
 
 const routes = Router();
 
 routes.post('/usuarios', UserController.create);
 
+routes.use(auth)
+
+routes.get('/', (req, res) => {
+    return res.send('Safe House');
+})
 export default routes;
