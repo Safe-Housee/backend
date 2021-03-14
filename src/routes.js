82342ab98635b93a08c942852dad4568e37c2235
Cristian Silva <cristian123105@gmail.com>
2021-03-14 11:53:19 -0300

    fix: create date

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import multer from "multer";
 import { auth } from "./middlewares/tokenValidation";
 import {
 	LoginController,
@@ -6,8 +7,10 @@ import {
 	UserController,
 	FileController,
 } from "./controllers";
+import multerConfig from "./config/multer";
 
 const routes = Router();
+const upload = multer(multerConfig);
 
 routes.get("/", (req, res) => res.send("Safe House"));
 routes.post("/login", LoginController.store);
@@ -26,5 +29,5 @@ routes.patch(
 routes.get("/partidas", MatchController.index);
 routes.get("/partidas/:partidaId", MatchController.getOne);
 // Salvar imagens
-routes.post("/uploadImage", FileController.store);
+routes.post("/uploadImage", upload.single("file"), FileController.store);
 export default routes;
