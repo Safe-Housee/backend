cbdd002bbda7ebaeae6fe4ed6bcbde83ca61f75d
Cristian Silva <cristian123105@gmail.com>
2021-05-03 21:31:02 -0300

    feat: salvando reporte

--- src/controllers/ReporteController.js
+++ src/controllers/ReporteController.js
@@ -1,17 +1,26 @@
+import { criarReporte } from "../services/reporteService";
+
 class ReporteController {
 	async store(req, res) {
-		const basicInformation = {
-			nm_reportador: "o nome do usuário que está reportando",
-			nm_reportado: "o nome do usuário reportado",
-			ds_reporte: "a descrição do motivo do reporte",
-		};
-		for (const info of Object.keys(basicInformation)) {
-			if (!req.body[info])
-				return res
-					.status(400)
-					.json({ message: `Deve enviar ${basicInformation[info]}` });
+		try {
+			const basicInformation = {
+				nm_reportador: "o nome do usuário que está reportando",
+				nm_reportado: "o nome do usuário reportado",
+				ds_reporte: "a descrição do motivo do reporte",
+			};
+			for (const info of Object.keys(basicInformation)) {
+				if (!req.body[info])
+					return res
+						.status(400)
+						.json({ message: `Deve enviar ${basicInformation[info]}` });
+			}
+			const reporteId = await criarReporte(req.body);
+			const response = { ...req.body, cd_reporte: reporteId };
+			return res.status(201).send(response);
+		} catch (error) {
+			console.error(error);
+			return res.status(500).send({ message: "Internal server error" });
 		}
-		return res.status(201).send("not implemented");
 	}
 }
 
---
+++ src/services/reporteService.js
@@ -0,0 +1,38 @@
+import { createConnection } from "../database/connection";
+
+export const criarReporte = async (reporteValues) => {
+	try {
+		const database = await createConnection();
+		const cd_reportado = await database.execute(
+			`
+            SELECT cd_usuario FROM tb_usuario WHERE nm_usuario = ?
+        `,
+			[reporteValues.nm_reportado]
+		);
+		const cd_reportador = await database.execute(
+			`
+            SELECT cd_usuario FROM tb_usuario WHERE nm_usuario = ?
+        `,
+			[reporteValues.nm_reportador]
+		);
+		const date = new Date();
+		const dataReporte = `${date.getFullYear()}/${date.getMonth()}/${date.getDay()}`;
+		const [[insertedReporte]] = await database.execute(
+			`
+            INSERT INTO
+                tb_reporte (
+                    cd_reportado,
+                    cd_reportador,
+                    dt_reporte,
+                    ds_statusReporte,
+                )
+            VALUES (?, ?, ?, ?)
+        `,
+			[cd_reportador, cd_reportado, dataReporte, reporteValues.ds_Reporte]
+		);
+		return insertedReporte.insertId;
+	} catch (error) {
+		console.error(error);
+		return new Error(error);
+	}
+};
