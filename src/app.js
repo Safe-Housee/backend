29b1fff4ce6de14787d60aea89030207354028f2
Cristian Silva <cristian123105@gmail.com>
2021-07-03 15:53:10 -0300

    chore: config socket io

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
+import { Server } from "socket.io";
+import serverHttp from "http";
 import routes from "./routes";
 
 const app = express();
+const http = serverHttp.createServer(app);
+const io = new Server(http, {
+	path: "/chat",
+	cors: {
+		origin: "http://localhost:4200",
+	},
+});
 
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(cors());
 app.use(routes);
+
+io.on("connection", (socket) => {
+	console.log("conectado", socket);
+	socket.on("message", (data) => {
+		console.log(data);
+	});
+});
 
 const port = process.env.PORT || 3333;
 
 if (process.env.NODE_ENV !== "test") {
-	app.listen(port, () => {
+	http.listen(port, () => {
 		console.log(`🏠 Server running on port: ${port}`);
 	});
 }
