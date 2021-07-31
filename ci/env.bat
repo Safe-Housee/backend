3ffd702ab32136a80565aa1c449e8890fc9ce746
Cristian Silva <cristian123105@gmail.com>
2021-07-31 15:42:59 -0300

    ci: teste heroku manual run

---
+++ ci/env.bat
@@ -0,0 +1,4 @@
+sed -i "s/process.env.DB_USER/'bYD4ZDMsh1'/g"  ./src/config/config.js
+sed -i "s/process.env.DB_NAME/'bYD4ZDMsh1'/g"  ./src/config/config.js
+sed -i "s/process.env.DB_PASS/'FgOmcxE5NS'/g"  ./src/config/config.js
+sed -i "s/process.env.DB_HOST/'remotemysql.com'/g"  ./src/config/config.js
